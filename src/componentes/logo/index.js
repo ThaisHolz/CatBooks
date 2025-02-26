5dd6a17f0f6a38d1d14ec8bb40348057e2614cbe
Thaís Cristine Coelho <thaisholz@hotmail.com>
2025-02-26 14:33:10 +0000

    Criando o primeiro componente

---
+++ src/componentes/logo/index.js
@@ -0,0 +1,12 @@
+import logo from '../../imagens/logo-catbooks.png';
+
+function Logo(){
+    return(
+        <div className='logo'>
+            <img src={logo} alt='logo'></img>
+            <p>CatBooks</p>
+       </div>
+    )
+}
+
+export default Logo;
