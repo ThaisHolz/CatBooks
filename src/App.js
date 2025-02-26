ae71a60a1707c27d8a89f9882ef5e7444a397255
Thaís Cristine Coelho <thaisholz@hotmail.com>
2025-02-26 14:17:25 +0000

    Começando o header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import logo from './imagens/logo-catbooks.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      
+    <div className='App'>
+      <header className='App-header'>
+      <div className='logo'>
+        <img src={logo} alt='logo'></img>
+        <p>CatBooks</p>
+      </div>
       </header>
     </div>
   );
