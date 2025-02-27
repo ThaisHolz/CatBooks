5464efd03ad8d307ec03a7565b624beeed762a69
Thaís Cristine Coelho <thaisholz@hotmail.com>
2025-02-27 14:21:52 +0000

    Utilizando styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
 import Header from './componentes/header/header.js';
+import styled from 'styled-components';
+
+ /* Substituição do css */ 
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(#E3D0D8, #827081);
+
+  li{
+    list-style: none;
+  }
+`
 
 function App() {
   return (
-    <div className='App'>
+    <AppContainer>
       <Header />
-    </div>
+    </AppContainer>
   );
 }
 
--- src/componentes/header/header.js
+++ src/componentes/header/header.js
@@ -1,15 +1,21 @@
 import Logo from '../logo';
 import Menu from '../menu';
 import IconeMenu from '../icones';
-import './header.css';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.header`
+    background-color: #FFF;
+    display: flex;
+    justify-content: center;
+`
 
 function Header(){
     return(
-        <header className='App-header'>
+        <HeaderContainer>
             <Logo/>
             <Menu/>
             <IconeMenu/>
-        </header>
+        </HeaderContainer>
     )
 }
 
